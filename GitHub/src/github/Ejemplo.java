a2552e27e083018207d6436f3358d8e0fb65ecdb
Alvaro Ramas Franco <dam1aalvaroramasfranco@iesantoniogala.es>
2021-03-18 13:16:01 +0100

    He añadido una lista al codigo

--- GitHub/src/github/Ejemplo.java
+++ GitHub/src/github/Ejemplo.java
@@ -1,9 +1,12 @@
 package github;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ejemplo {
 
 	public static void main(String[] args) {
 	System.out.println("Hola a todos");
+	List<Integer> lista = new ArrayList<Integer>();
 	}
-
 }
